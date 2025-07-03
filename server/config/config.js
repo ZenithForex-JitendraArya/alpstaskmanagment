9324bede8111d1cc0188a2d5c9e6feae533bcb50
JitendraArya <it.developer1@zenithforex.com>
2025-07-03 16:59:04 +0530

    env updated

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,42 @@
 require('dotenv').config(); // so it can read .env
 
+// module.exports = {
+//     development: {
+//         username: process.env.USER,
+//         password: process.env.PASSWORD,
+//         database: process.env.DATABASE,
+//         host: process.env.HOST,
+//         dialect: 'postgres',
+//         logging: false,
+//     },
+//     development: {
+//         username: process.env.USER,
+//         password: process.env.PASSWORD,
+//         database: process.env.DATABASE,
+//         host: process.env.HOST,
+//         dialect: 'postgres',
+//         logging: false,
+//     },
+// };
 module.exports = {
     development: {
-        username: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        host: process.env.HOST,
+        use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
-        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     },
+    production: {
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
 };
